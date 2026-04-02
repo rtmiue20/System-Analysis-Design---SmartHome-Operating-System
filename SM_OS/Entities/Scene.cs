7bf20ec0a701ce5856be72c3bee03c63f3f7ca47
MinhTrieu <trieunguyen.31241027455@st.ueh.edu.vn>
2026-04-02 13:32:40 +0700

    BS Lớp Scene và SceneAction

---
+++ SM_OS/Entities/Scene.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+/// <summary>
+/* Lớp Scene đại diện cho một ngữ cảnh trong hệ thống, bao gồm các hành động liên quan đến một ngữ cảnh cụ thể
+(ví dụ: "Đi ngủ", "Về nhà") mà người dùng có thể thiết lập để tự động hóa các thiết bị thông minh trong nhà.
+*/
+/// </summary>
+public class Scene
+{
+	public Scene()
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty; // "Đi ngủ", "Về nhà"...
+
+        public int UserId { get; set; } // Ngữ cảnh này của User nào
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null!;
+
+        // Một ngữ cảnh gồm nhiều hành động (ví dụ: bật đèn 1, tắt quạt 2)
+        public ICollection<SceneAction> SceneActions { get; set; } = new List<SceneAction>();   
+    }
+}
---
+++ SM_OS/Entities/SceneAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+/// <summary>
+/// Lớp SceneAction đại diện cho một hành động cụ thể trong một ngữ cảnh (Scene),
+/// liên kết với một thiết bị thông minh cụ thể và trạng thái mong muốn của thiết bị đó khi ngữ cảnh được kích hoạt.
+/// </summary>
+public class SceneAction
+{
+	public SceneAction()
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TargetStatus { get; set; } = string.Empty; // "On", "Off", "70%"...
+
+        public int SceneId { get; set; }
+        [ForeignKey("SceneId")]
+        public Scene Scene { get; set; } = null!;
+
+        public int SmartDeviceId { get; set; }
+        [ForeignKey("SmartDeviceId")]
+        public SmartDevice SmartDevice { get; set; } = null!;
+    }
+}
